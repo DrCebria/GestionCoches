39bd2827353d7f47ad3f1ad13952195ebe4ece6a
DrCebria <gerardcebria@gmail.com>
2016-05-06 09:48:09 +0200

    Parque

--- Documents/Proyectos Github/Coches/src/coches/GestionCoches.java
+++ Documents/Proyectos Github/Coches/src/coches/GestionCoches.java
@@ -5,12 +5,17 @@
  */
 package coches;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.StringTokenizer;
 import javax.swing.table.DefaultTableModel;
 
 /**
@@ -21,18 +26,22 @@ public class GestionCoches {
 
     private Connection conexion;
     String direccion = "jdbc:mysql://localhost:3306/parques";
+    String titulos[] = {"Matricula", "Marca", "Modelo", "Color", "Ano", "Precio"};
+    String filas[] = new String [6];
     String createTable = "CREATE TABLE coches ("
-            + "  Matricula varchar(7) NOT NULL,"
+            + "  Matricula varchar(45) NOT NULL,"
             + "  Marca varchar(45) NOT NULL,"
             + "  Modelo varchar(45) NOT NULL,"
             + "  Color varchar(45) NOT NULL,"
-            + "  A?o int(11) NOT NULL,"
+            + "  Ano int(11) NOT NULL,"
             + "  Precio int(11) NOT NULL,"
             + "  PRIMARY KEY (Matricula)"
             + ") ENGINE=InnoDB DEFAULT CHARSET=utf8;";
     DefaultTableModel modelo = new DefaultTableModel(null, titulos);
     static PreparedStatement ps;
     static ResultSet rs;
+    static String linea;
+    static String[] trozos;
 
     public Connection crearConexion() {
         try {
@@ -52,6 +61,7 @@ public class GestionCoches {
 
             ps = conexion.prepareStatement(createTable);
             rs = ps.executeQuery();
+            comp = true;
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
@@ -59,8 +69,23 @@ public class GestionCoches {
         return comp;
     }
 
-    public String cargarTablaCoches(File fichero) {
-        return "SHUT UP";
+    public int cargarTablaCoches(File fichero) {
+        int contador = 0;
+        try (FileReader fr = new FileReader(fichero)) {
+            BufferedReader br = new BufferedReader(fr);
+            while (br.readLine() != null) {
+                contador++;
+                linea = br.readLine();
+                trozos = linea.split(":");
+                
+                
+            }
+            
+        } catch (IOException ex) {
+            throw new RuntimeException(ex);
+
+        }
+        return contador;
     }
 
     public String[][] mostrarCoches(String sql) {
