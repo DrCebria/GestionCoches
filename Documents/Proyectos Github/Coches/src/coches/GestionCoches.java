736a6e7f7bf724310640f30fa2ef457ef91d6d83
DrCebria <gerardcebria@gmail.com>
2016-05-10 08:51:01 +0200

    Parque

--- Documents/Proyectos Github/Coches/src/coches/GestionCoches.java
+++ Documents/Proyectos Github/Coches/src/coches/GestionCoches.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package coches;
 
 import java.io.BufferedReader;
@@ -16,16 +11,23 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.StringTokenizer;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.swing.table.DefaultTableModel;
 
 /**
+ * Esta clase reúne todos los métodos necesarios para la gestión de los coches
  *
- * @author daw1
+ * @author GerardCebriá
  */
 public class GestionCoches {
 
+    public GestionCoches(Connection conexion) {
+        this.conexion = conexion;
+    }
+    
     private Connection conexion;
-    String direccion = "jdbc:mysql://localhost:3306/parques";
+    String direccion = "jdbc:mysql://localhost:3306/coches";
     String titulos[] = {"Matricula", "Marca", "Modelo", "Color", "Ano", "Precio"};
     String filas[] = new String[6];
     String createTable = "CREATE TABLE coches ("
@@ -37,12 +39,19 @@ public class GestionCoches {
             + "  Precio int(11) NOT NULL,"
             + "  PRIMARY KEY (Matricula)"
             + ") ENGINE=InnoDB DEFAULT CHARSET=utf8;";
+
     DefaultTableModel modelo = new DefaultTableModel(null, titulos);
 
+    /**
+     * El método consiste en establecer la conexión con la base de datos SQL
+     * "coches"
+     *
+     * @return Conection
+     */
     public Connection crearConexion() {
-        
+
         try {
-            conexion = DriverManager.getConnection(direccion, "root", "root");
+            this.conexion = DriverManager.getConnection(direccion, "root", "root");
 
         } catch (SQLException e) {
             //LabelConexion.setText(e.getMessage() + " -- " + e.getErrorCode());
@@ -52,14 +61,21 @@ public class GestionCoches {
         return conexion;
     }
 
+    /**
+     * En este método se pretende crear la tabla en el archivo SQL
+     *
+     * @return g<strong> true </strong> si se ha completado,
+     * <strong>false</strong>
+     * si no se ha creado.
+     */
     public boolean crearTablaCoches() {
         PreparedStatement ps;
-        ResultSet rs;
+        
         boolean comp = false;
         try {
 
             ps = conexion.prepareStatement(createTable);
-            rs = ps.executeQuery();
+            ps.execute();
             comp = true;
         } catch (SQLException e) {
             throw new RuntimeException(e);
@@ -68,7 +84,15 @@ public class GestionCoches {
         return comp;
     }
 
-    public int cargarTablaCoches(File fichero) {
+    /**
+     * El siguiente método carga en la tabla el contenido del fichero pasado
+     * como parámetro.
+     *
+     * @param fichero Un <code> file</code> del que sacaremos el texto.
+     * @return número de coches que hay
+     */
+    public int cargarTablaCoches(File fichero) throws SQLException {
+        String matricula, marca, modelo, color, ano, precio;
         PreparedStatement ps;
         ResultSet rs;
         String linea;
@@ -78,22 +102,37 @@ public class GestionCoches {
             BufferedReader br = new BufferedReader(fr);
             while ((linea = br.readLine()) != null) {
                 contador++;
-                
+
                 trozos = linea.split(":");
-                for (String t : trozos) {
-
-                }
-
+                matricula = trozos[0];
+                marca = trozos[1];
+                modelo = trozos[2];
+                color = trozos[3];
+                ano = trozos[4];
+                precio = trozos[5];
+                ps = conexion.prepareStatement("INSERT INTO coches values ('?', '?', '?', '?', '?')");
+                ps.setString(1, matricula);
+                ps.setString(2, marca);
+                ps.setString(3, modelo);
+                ps.setString(4, color );
+                ps.setString(5, ano);
+                ps.setString(6, precio);
+                
+                rs = ps.executeQuery();
             }
-
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(GestionCoches.class.getName()).log(Level.SEVERE, null, ex);
         } catch (IOException ex) {
-            throw new RuntimeException(ex);
-
+            Logger.getLogger(GestionCoches.class.getName()).log(Level.SEVERE, null, ex);
         }
         return contador;
     }
-
-    public String[][] mostrarCoches(String sql) {
-
-    }
 }
+
+
+
+//public String[][] mostrarCoches(String sql) {
+//
+//
+//    }
+
